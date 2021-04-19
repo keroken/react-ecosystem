e43b2086f981be812abbb1d6358382074620408b
keroken <kenichi.shinagawa@gmail.com>
2021-04-19 23:49:48 +0900

    add persist storage

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -5,7 +5,10 @@ const TodoListItem = ({ todo, onRemovePressed }) => (
   <StyledItemLine className="todo-item-container">
     <h3>{todo.text}</h3>
     <div className="buttons-container">
-      <button className="completed-button">Mark As Completed</button>
+      {todo.isCompleted
+        ? null
+        : <button className="completed-button">Mark As Completed</button>
+      }
       <button
         onClick={() => onRemovePressed(todo.text)} 
         className="remove-button"
