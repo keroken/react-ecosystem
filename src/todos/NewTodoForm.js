88f227ff3aaf2c7f28cc7486ffefefcd69f9533f
keroken <kenichi.shinagawa@gmail.com>
2021-04-19 21:31:35 +0900

    base project

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <div className="new-todo-form">
+      <input
+        className="new-todo-input"
+        type="text"
+        placeholder="type todos"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button className="new-todo-button">Create Todo</button>
+    </div>
+  );
+};
+
+export default NewTodoForm;
