f6a66d487aac90fb983f23272a16de73ce5194ca
keroken <kenichi.shinagawa@gmail.com>
2021-04-25 11:20:18 +0900

    add async thunk

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,19 @@
-export const displayAlert = () => () => {
-  alert('Hello');
+import { LOAD_TODOS_SUCCESS, LOAD_TODOS_FAILURE, LOAD_TODOS_IN_PROGRESS, loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://loaclhost:8080/todos');
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+  
+}
+
+export const displayAlert = text => () => {
+  alert(text);
 };
